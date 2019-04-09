6fd460d94cb313a108b864d03d3a069671ab0771
Ray Chao <ray.chao@xuenn.com>
2019-04-09 16:25:00 +0800

    [ObserverPattern] move BoUserEmailService to new file

---
+++ ObserverPattern/Services/BoUserEmailService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ObserverPattern.Interfaces;
+using ObserverPattern.Models;
+
+namespace ObserverPattern.Services
+{
+    public class BoUserEmailService : IEmailNotificationObserver
+    {
+        public void SendEmail(Member member)
+        {
+            //TODO: Send email to Back Office user
+            Console.WriteLine($"Send email to BO admin, {member.MemberCode}'s balance has been changed, current balance: {member.Balance}");
+        }
+    }
+}
--- ObserverPattern/Services/MemberEmailService.cs
+++ ObserverPattern/Services/MemberEmailService.cs
@@ -12,13 +12,4 @@ namespace ObserverPattern.Services
             Console.WriteLine($"Send email to {member.MemberCode}, email: {member.Email}, balance: {member.Balance}");
         }
     }
-
-    public class BoUserEmailService : IEmailNotificationObserver
-    {
-        public void SendEmail(Member member)
-        {
-            //TODO: Send email to Back Office user
-            Console.WriteLine($"Send email to BO admin, {member.MemberCode}'s balance has been changed, current balance: {member.Balance}");
-        }
-    }
 }
