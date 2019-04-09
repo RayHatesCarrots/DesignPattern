edb476fad1c520d9e8c2fddb0db99faa08bfd54d
Ray Chao <ray.chao@xuenn.com>
2019-04-09 17:20:07 +0800

    [FactoryMethodPattern] initial Factory Method Pattern sample code

---
+++ FactoryMethodPattern/ConnectionFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using MongoDB.Driver;
+using MySql.Data.MySqlClient;
+
+namespace FactoryMethodPattern
+{
+    public static class ConnectionFactory
+    {
+        public static IDbConnection Db1(bool open = true)
+        {
+            var db1 = new SqlConnection();
+            Console.WriteLine("Connection DB1(MSSQL) initialized");
+
+            if (open)
+            {
+                //db1.Open();
+                Console.WriteLine("Connection DB1(MSSQL) open");
+            }
+
+            return db1;
+        }
+
+        public static IDbConnection Db2(bool open = true)
+        {
+            var db2 = new MySqlConnection();
+            Console.WriteLine("Connection DB2(MySQL) initialized");
+
+            if (open)
+            {
+                //db2.Open();
+                Console.WriteLine("Connection DB2(MySQL) open");
+            }
+
+            return db2;
+        }
+
+        public static MongoClient Db3()
+        {
+            var db3 = new MongoClient();
+            Console.WriteLine("Connection DB3(MongoDB) initialized");
+
+            return db3;
+        }
+    }
+}
---
+++ FactoryMethodPattern/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.Common;
+
+namespace FactoryMethodPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=========== Factory Method Pattern ===========");
+
+            using (var db1 = ConnectionFactory.Db1())
+            {
+                //TODO: do something for DB1
+            }
+
+            using (var db2 = ConnectionFactory.Db2())
+            {
+                //TODO: do something for DB2
+            }
+
+            var db3 = ConnectionFactory.Db3();
+            //TODO: do something for DB3
+
+            Console.WriteLine("=========== Factory Method Pattern(END) ===========");
+            Console.ReadLine();
+        }
+    }
+}
